6344133b3672f6fd38d90a574317cf9b02d05c08
melashry <mohamedelashry323@gmail.com>
2017-07-29 20:26:56 +0200

    intial

--- app/src/main/java/com/example/elashry/eleman/Activities/Category.java
+++ app/src/main/java/com/example/elashry/eleman/Activities/Category.java
@@ -11,15 +11,25 @@ import com.example.elashry.eleman.R;
 public class Category extends AppCompatActivity {
 
 
-    ImageView img;
+    ImageView img ,img2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
         img = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
 
         img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(Category.this,Detail.class);
+                startActivity(i);
+            }
+        });
+
+
+        img2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i =new Intent(Category.this,ItemCategory.class);

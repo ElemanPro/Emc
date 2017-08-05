13c78ece4097c0566aa1c2a7638c2e3fa34971c8
melashry <mohamedelashry323@gmail.com>
2017-08-06 01:54:30 +0200

    intial

---
+++ app/src/main/java/com/example/elashry/eleman/Adapter/MaintenanceAdapter.java
@@ -0,0 +1,79 @@
+package com.example.elashry.eleman.Adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.elashry.eleman.Model.Product_Model;
+import com.example.elashry.eleman.R;
+
+import java.util.List;
+
+/**
+ * Created by elashry on 8/6/2017.
+ */
+
+public class MaintenanceAdapter extends RecyclerView.Adapter <MaintenanceAdapter.ViewHoler>{
+
+    private Context mContext;
+    LayoutInflater inflater;
+    private List<Product_Model> pro_List;
+
+    public MaintenanceAdapter(Context mContext,List<Product_Model> pro_List) {
+        this.mContext = mContext;
+        this.pro_List =pro_List;
+        inflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public MaintenanceAdapter.ViewHoler onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.product_row,parent,false);
+        MaintenanceAdapter.ViewHoler holer = new MaintenanceAdapter.ViewHoler(view);
+        return holer;
+    }
+
+    @Override
+    public void onBindViewHolder(MaintenanceAdapter.ViewHoler holder, int position) {
+        // Picasso.with(mContext).load(Uri.parse(pro_List.get(position).getPro_Image_url().toString())).into(holder.product_image);
+
+        Toast.makeText(mContext,pro_List.get(position).getPro_Image_url().toString() , Toast.LENGTH_SHORT).show();
+        holder.product_image.setImageBitmap(getBitmapFromString(pro_List.get(position).getPro_Image_url().toString()));
+        holder.product_categ.setText(pro_List.get(position).getPro_Categ().toString());
+        holder.product_name.setText(pro_List.get(position).getPro_Name().toString());
+        holder.product_price.setText(pro_List.get(position).getPro_Price().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return pro_List.size();
+    }
+    class ViewHoler extends RecyclerView.ViewHolder{
+        ImageView product_image;
+        TextView product_categ;
+        TextView product_name;
+        TextView product_price;
+        public ViewHoler(View itemView) {
+            super(itemView);
+            product_image     = (ImageView) itemView.findViewById(R.id.product_image);
+            product_categ     = (TextView) itemView.findViewById(R.id.product_categ);
+            product_name      = (TextView) itemView.findViewById(R.id.product_name);
+            product_price     = (TextView) itemView.findViewById(R.id.product_price);
+
+        }
+
+    }
+    private Bitmap getBitmapFromString(String imageString)
+    {
+        byte [] arr = Base64.decode(imageString,Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(arr,0,arr.length);
+        return bitmap;
+    }
+}
---
+++ app/src/main/java/com/example/elashry/eleman/Model/MaintenanceModel.java
@@ -0,0 +1,8 @@
+package com.example.elashry.eleman.Model;
+
+/**
+ * Created by elashry on 8/6/2017.
+ */
+
+public class MaintenanceModel {
+}

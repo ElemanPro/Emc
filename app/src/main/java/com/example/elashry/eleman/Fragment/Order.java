a0a9569730f6ee5abb68c29c48d0aeac31340c34
Emadmagdy151995Fci <emadmagdi.151995@gmai.com>
2017-08-06 02:36:14 +0200

    third_update

--- app/src/main/java/com/example/elashry/eleman/Fragment/Order.java
+++ app/src/main/java/com/example/elashry/eleman/Fragment/Order.java
@@ -1,16 +1,25 @@
 package com.example.elashry.eleman.Fragment;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.elashry.eleman.Controller;
 import com.example.elashry.eleman.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by Delta on 06/08/2017.
@@ -26,7 +35,13 @@ public class Order extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.order,container,false);
         init_View(view);
+        GetOrder_Data();
         return view;
+    }
+
+    private void GetOrder_Data() {
+        new Asyn_task().execute(order_url);
+
     }
 
     private void init_View(View view) {
@@ -34,4 +49,32 @@ public class Order extends Fragment {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.order_RecyView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
     }
+    private class Asyn_task extends AsyncTask<String, Void,Void>
+    {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            JsonArrayRequest mJsonArrayRequest = new JsonArrayRequest(strings[0],
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            Log.e("data",response.toString());
+                            JSONObject object;
+
+                        }
+                    }
+                    ,
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+            Controller.getInstance().addToRequestQueue(mJsonArrayRequest,"json array req");
+            return null;
+        }
+
+    }
+
 }

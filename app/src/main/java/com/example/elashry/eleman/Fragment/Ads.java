498709f310a272846b8fbe9ac20bb70c58c1227e
melashry <mohamedelashry323@gmail.com>
2017-08-19 13:07:59 +0200

    intial

---
+++ app/src/main/java/com/example/elashry/eleman/Fragment/Ads.java
@@ -0,0 +1,8 @@
+package com.example.elashry.eleman.Fragment;
+
+/**
+ * Created by elashry on 8/16/2017.
+ */
+
+public class Ads {
+}
--- app/src/main/java/com/example/elashry/eleman/Model/AdvertsmentModel.java
+++ app/src/main/java/com/example/elashry/eleman/Model/AdvertsmentModel.java
@@ -9,11 +9,14 @@ public class AdvertsmentModel {
     private String ads_name ;
     private String ads_details;
     private String ads_image;
+    private String ads_date;
 
-    public AdvertsmentModel(String ads_name, String ads_details, String ads_image) {
+    public AdvertsmentModel(String ads_name, String ads_details, String ads_image, String ads_date) {
         this.ads_name = ads_name;
         this.ads_details = ads_details;
         this.ads_image = ads_image;
+        this.ads_date = ads_date;
+
     }
 
     public String getAds_name() {
@@ -26,5 +29,9 @@ public class AdvertsmentModel {
 
     public String getAds_image() {
         return ads_image;
+    }
+
+    public String getAds_date() {
+        return ads_date;
     }
 }

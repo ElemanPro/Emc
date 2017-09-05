fd187fb2ccf6c427a1a75f057bc93eb547a56386
melashry <mohamedelashry323@gmail.com>
2017-09-06 00:34:54 +0200

    intial commit

--- app/src/main/java/com/example/elashry/eleman/App_URL.java
+++ app/src/main/java/com/example/elashry/eleman/App_URL.java
@@ -3,17 +3,17 @@ package com.example.elashry.eleman;
 
 
 public class App_URL {
-    public static final String product_url           ="http://semicolonsoft.com/clients/emc/api/find/products";
+    public static final String product_url           ="http://emcmisir.com/api/find/products";
     //public static final String advertisement         ="http://semicolonsoft.com/clients/emc/api/find/advertisement";
-    public static final String advertisement         ="http://semicolonsoft.com/clients/emc/api/ads";
-    public static final String app_maintenance       ="http://semicolonsoft.com/clients/emc/api/find/app_maintenance";
-    public static final String add_maintenance_order ="http://semicolonsoft.com/clients/emc/api/addmentanaceorder";
-    public static final String matgar_order          ="http://semicolonsoft.com/clients/emc/api/matgarorders";
-    public static final String add_itemCateg_data    ="http://semicolonsoft.com/clients/emc/api/addorders";
-    public static final String app_matgar            ="http://semicolonsoft.com/clients/emc/api/find/app_matgar";
-    public static final String app_orders            ="http://semicolonsoft.com/clients/emc/api/find/app_orders";
-    public static final String tokens                ="http://semicolonsoft.com/clients/emc/api/find/tokens";
-    public static final String image_url             ="http://semicolonsoft.com/clients/emc/public/uploads/images/";
+    public static final String advertisement         ="http://emcmisir.com/api/ads";
+    public static final String app_maintenance       ="http://emcmisir.com/api/find/app_maintenance";
+    public static final String add_maintenance_order ="http://emcmisir.com/api/addmentanaceorder";
+    public static final String matgar_order          ="http://emcmisir.com/api/matgarorders";
+    public static final String add_itemCateg_data    ="http://emcmisir.com/api/addorders";
+    public static final String app_matgar            ="http://emcmisir.com/api/find/app_matgar";
+    public static final String app_orders            ="http://emcmisir.com/api/find/app_orders";
+    public static final String tokens                ="http://emcmisir.com/api/find/tokens";
+    public static final String image_url             ="http://emcmisir.com/public/uploads/images/";
     //larg image good
     //http://semicolonsoft.com/clients/emc/public/uploads/images/
     //small image bad
